e49865743eb5d464704ed7c40685dabe33440c00
hsn0najafi <hsn0najafi@gmail.com>
2023-05-30 01:48:40 +0330

    feat(core): inject microservice client into controller

--- core/src/auth/auth.controller.ts
+++ core/src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Inject,
+  // Version,
+  // VERSION_NEUTRAL,
+} from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Controller({ path: 'auth', version: '1' })
-export class AuthController {}
+export class AuthController {
+  constructor(
+    @Inject('AUTH_MICROSERVICE')
+    private readonly client: ClientKafka,
+  ) {}
+}
