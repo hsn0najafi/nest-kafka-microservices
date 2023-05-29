bacbbad47719ebfc8d7ad5c5a607158c78bfe102
hsn0najafi <hsn0najafi@gmail.com>
2023-05-29 22:35:13 +0330

    chore(): convert application to a microservice

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { logLevel } from 'kafkajs';
+import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app.module';
 
+const configService: ConfigService = new ConfigService();
+
 (async () => {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: configService.get('KAFKA_BROKERS').split(','),
+          logLevel: logLevel.ERROR,
+          connectionTimeout: 1000,
+        },
+        consumer: {
+          groupId: configService.get('KAFKA_GROUP_ID'),
+          heartbeatInterval: 1000,
+          maxWaitTimeInMs: 1000,
+          rebalanceTimeout: 1000,
+        },
+      },
+    },
+  );
+  await app.listen();
+  console.log(`\n---------=+ Auth is Running +=---------\n`);
 })();
