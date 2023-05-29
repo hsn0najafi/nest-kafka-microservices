be52a9a80ffd4ff93a61a8d17a110d4dc895c903
hsn0najafi <hsn0najafi@gmail.com>
2023-05-30 02:55:21 +0330

    feat(core): create a testing protected route for testig JWT

--- core/src/auth/auth.controller.ts
+++ core/src/auth/auth.controller.ts
@@ -1,15 +1,20 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   Inject,
   Post,
+  UseGuards,
   // Version,
   // VERSION_NEUTRAL,
 } from '@nestjs/common';
 import { ClientKafka, RpcException } from '@nestjs/microservices';
+import { ApiBearerAuth } from '@nestjs/swagger';
 import { catchError, Observable, throwError, timeout } from 'rxjs';
+import { UserId } from 'src/shared/decorators';
+import { AuthGuard } from 'src/shared/guards';
 
 import { LoginDto, RegisterDto } from './dto';
 
@@ -35,6 +40,17 @@ export class AuthController {
   @Post('login')
   login(@Body() loginDto: LoginDto): Observable<any> {
     return this.client.send('login', { body: loginDto }).pipe(
+      timeout(400),
+      catchError((err) => throwError(() => new RpcException(err.response))),
+    );
+  }
+
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get('me')
+  me(@UserId() userId): Observable<any> {
+    return this.client.send('me', { body: userId }).pipe(
       timeout(400),
       catchError((err) => throwError(() => new RpcException(err.response))),
     );
