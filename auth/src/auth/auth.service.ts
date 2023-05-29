fa12f7161904be9dbacc0ad1d86b79660e3fda41
hsn0najafi <hsn0najafi@gmail.com>
2023-05-29 23:33:30 +0330

    chore(): complete authentication and Authorization routes

--- auth/src/auth/auth.service.ts
+++ auth/src/auth/auth.service.ts
@@ -1,23 +1,107 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { RpcException } from '@nestjs/microservices';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 import { RegisterDto } from './dto';
 import { LoginDto } from './dto';
+import { User } from './entities';
+import { comparePassword, hashPassword } from '../utils';
 
 @Injectable()
 export class AuthService {
-  async register(user: RegisterDto) {
-    return;
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register({ username, password, name }: RegisterDto) {
+    try {
+      const userExist = await this.usersRepository.findOneBy({ username });
+      if (userExist) {
+        throw new ConflictException('Username is already exist.');
+      }
+
+      const hash = await hashPassword(password);
+      const user = await this.usersRepository.save({
+        username,
+        password: hash,
+        name,
+      });
+
+      return {
+        id: user.id,
+        username,
+        name,
+      };
+    } catch (err) {
+      throw new RpcException(err);
+    }
   }
 
-  async login(user: LoginDto) {
-    return;
+  async login({ username, password }: LoginDto) {
+    try {
+      const user = await this.usersRepository.findOneBy({ username });
+      if (!user) {
+        throw new ForbiddenException('Username or password are not correct.');
+      }
+
+      const validPassword = await comparePassword(password, user.password);
+      if (!validPassword) {
+        throw new ForbiddenException('Username or password are not correct.');
+      }
+
+      const accessToken = this.jwtService.sign({
+        id: user.id,
+        username,
+        role: 'user',
+      });
+
+      return {
+        accessToken,
+      };
+    } catch (err) {
+      throw new RpcException(err);
+    }
   }
 
   isAuthenticated(jwt: string) {
-    return;
+    try {
+      if (this.jwtService.verify(jwt)) {
+        return 'yes';
+      }
+      return 'no';
+    } catch (err) {
+      return 'no';
+    }
   }
 
   async me(userId: string) {
-    return;
+    try {
+      const user = await this.usersRepository.findOneBy({
+        id: +userId,
+      });
+
+      if (!user) {
+        throw new NotFoundException('User not found.');
+      }
+
+      const { id, name, username } = user;
+
+      return {
+        id,
+        name,
+        username,
+      };
+    } catch (err) {
+      throw new RpcException(err);
+    }
   }
 }
