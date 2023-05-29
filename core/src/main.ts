ab4c4f7fad841bf90eb98f1b31668e3543611d53
hsn0najafi <hsn0najafi@gmail.com>
2023-05-30 01:07:51 +0330

    chore(): add configService

--- core/src/main.ts
+++ core/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
 
+const configService: ConfigService = new ConfigService();
+
 (async () => {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const port = configService.get<number>('PORT') || 3000;
+  await app.listen(port);
+  console.log(`\n---------=+ Core is Running +=---------\n`);
 })();
