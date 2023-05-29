2210e2be0ed8da32ef490c07ce422593bf716859
hsn0najafi <hsn0najafi@gmail.com>
2023-05-29 22:50:10 +0330

    chore(): setup auth controller

--- auth/src/auth/auth.controller.ts
+++ auth/src/auth/auth.controller.ts
@@ -1,4 +1,34 @@
 import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+
+import { AuthService } from './auth.service';
+import { RegisterDto, LoginDto } from './dto';
 
 @Controller()
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @MessagePattern('register')
+  async register(@Payload('body') payload: RegisterDto) {
+    console.log('-------------- Register -------------');
+    return await this.authService.register(payload);
+  }
+
+  @MessagePattern('login')
+  async login(@Payload('body') payload: LoginDto) {
+    console.log('-------------- Auth / Login -------------');
+    return await this.authService.login(payload);
+  }
+
+  @MessagePattern('isAuthenticated')
+  async checkAccess(@Payload('body') payload: string) {
+    console.log('-------------- Auth / isAuthenticated -------------');
+    return this.authService.isAuthenticated(payload);
+  }
+
+  @MessagePattern('me')
+  async me(@Payload('body') payload: string) {
+    console.log('-------------- Auth / Me -------------');
+    return await this.authService.me(payload);
+  }
+}
--- auth/src/auth/auth.service.ts
+++ auth/src/auth/auth.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
+import { RegisterDto } from './dto';
+import { LoginDto } from './dto';
+
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async register(user: RegisterDto) {
+    return;
+  }
+
+  async login(user: LoginDto) {
+    return;
+  }
+
+  isAuthenticated(jwt: string) {
+    return;
+  }
+
+  async me(userId: string) {
+    return;
+  }
+}
