d16e251713090fd6f5121082030b10d7c525621a
hsn0najafi <hsn0najafi@gmail.com>
2023-05-30 02:28:08 +0330

    feat(core): add a register route

--- core/src/auth/auth.controller.ts
+++ core/src/auth/auth.controller.ts
@@ -1,10 +1,16 @@
 import {
+  Body,
   Controller,
+  HttpCode,
+  HttpStatus,
   Inject,
+  Post,
   // Version,
   // VERSION_NEUTRAL,
 } from '@nestjs/common';
-import { ClientKafka } from '@nestjs/microservices';
+import { ClientKafka, RpcException } from '@nestjs/microservices';
+import { catchError, Observable, throwError, timeout } from 'rxjs';
+import { RegisterDto } from './dto';
 
 @Controller({ path: 'auth', version: '1' })
 export class AuthController {
@@ -12,4 +18,13 @@ export class AuthController {
     @Inject('AUTH_MICROSERVICE')
     private readonly client: ClientKafka,
   ) {}
+
+  @HttpCode(HttpStatus.CREATED)
+  @Post('register')
+  register(@Body() registerDto: RegisterDto): Observable<any> {
+    return this.client.send('register', { body: registerDto }).pipe(
+      timeout(400),
+      catchError((err) => throwError(() => new RpcException(err.response))),
+    );
+  }
 }
